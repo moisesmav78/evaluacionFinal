4a52ea95d7b56fce429b5d5332f96ec0a64dfa8f
Moises Mavo <moisesmavo500@gmail.com>
2023-12-11 15:27:29 -0300

    Prueba

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+const jugadores = [
+    {jugador: "jugador_1", n1: 6, n2: 30, n3: 12, n4: 19, n5: 45, n6: 27}
+|   {jugador: "jugador_2", n2: 9, n2: 78, n3: 54, n4: 74, n5: 68, n6: 98}
+|   {jugador: "jugador_3", n3: 32, n2: 47, n3: 29, n4: 90, n5: 81, n6: 33}
+|   {jugador: "jugador_4", n4: 16, n2: 1, n3: 89, n4: 71, n5: 41, n6: 91}
+|   {jugador: "jugador_5", n5: 43, n2: 102, n3: 11, n4: 60, n5: 57, n6: 3}
+|   {jugador: "jugador_6", n6: 110, n2: 63, n3: 78, n4: 99, n5: 39, n6: 73}
+|   {jugador: "jugador_7", n7: 120, n2: 164, n3: 29, n4: 56, n5: 98, n6: 100}
+|   {jugador: "jugador_8", n: 200, n2: 354, n3: 91, n4: 187, n5: 42, n6: 130}   
+    ]
+    
+    const lista = document.querySelector('#loteria') 
+
+    const generarTemplate = () =>{
+        let template='';
+        let cant=0
+        for (const jugador of jugadores){
+            if (jugador.n1.n2.n3.n4.n5.n6.n7){
+                //cant = cant+1
+                cant++
+            }
+    
+       
+        }
+       
+    }
+    
